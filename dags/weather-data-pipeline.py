8f284f24cd6fc4e98c7cadef5501fa441225e428
savvas-pehli <savvaspehli@gmail.com>
2026-03-08 19:31:10 +0200

    fix: correct start date and namespace variables in Airflow orchestration

--- dags/weather-data-pipeline.py
+++ dags/weather-data-pipeline.py
@@ -11,33 +11,50 @@ from airflow import DAG
 
 default_args={'owner':'savvas',
               'retries':1,
-              'retry_delay':timedelta(minutes=1)}
+              'retry_delay':timedelta(minutes=1),
+              'depends_on_past':False,
+              'start_date':datetime(2026,3,8)}
 
 DAGS_DIR=os.path.dirname(__file__)
 PROJECT_ROOT=os.path.dirname(DAGS_DIR)
 
 with DAG(
-    dag_id='weather_medallion_pipeline',
+    dag_id='1_bronze_weather_ingestion',
     default_args=default_args,
-    schedule='0 10,14 * * 1-5',
-    start_date=datetime(2024,1,1),
+    schedule='*/10 * * * *',
     catchup=False
-    ) as dag:
+    ) as bronze_dag:
     
-    task_1=BashOperator(task_id='run_bronze',
-                        bash_command=f'{sys.executable} {os.path.join(PROJECT_ROOT,"Requesting_script.py")}',
-                        cwd=PROJECT_ROOT)
+    task_1=BashOperator(
+        task_id='run_bronze',
+        bash_command=f'{sys.executable} {os.path.join(PROJECT_ROOT,"Requesting_script.py")}',
+        cwd=PROJECT_ROOT
+        )
     
+    
+with DAG(dag_id='2_silver_weather_batching',
+    default_args=default_args,
+    schedule='0 * * * *',
+    catchup=False
+    ) as silver_dag:
+
     task_2=BashOperator(
         task_id='run_silver',
         bash_command=f'{sys.executable} {os.path.join(PROJECT_ROOT,"transform-silver.py")}',
-        cwd=PROJECT_ROOT     
+        cwd=PROJECT_ROOT
         )
     
-    task_3=BashOperator(
-        task_id='run_gold',
-        bash_command=f'{sys.executable} {os.path.join(PROJECT_ROOT,"transform-gold.py")}',
-        cwd=PROJECT_ROOT
-    )
-    task_1>>task_2>>task_3
+
+with DAG(dag_id='3_gold_weather_aggregation',
+    default_args=default_args,
+    schedule='0 0 * * *',
+    catchup=False
+    ) as gold_dag:
+      
+
+        task_3=BashOperator(
+            task_id='run_gold',
+            bash_command=f'{sys.executable} {os.path.join(PROJECT_ROOT,"transform-gold.py")}',
+            cwd=PROJECT_ROOT
+            )
     
